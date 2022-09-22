aeac98f5d903bb48875852bf6785f38f9b43c711
Soheibkend <soheibkenden@gmail.com>
2022-09-22 20:12:54 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import arrow from "./images/icon-arrow.svg";
 import background from "./images/pattern-bg.png";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from "./components/icon";
+import Markerposition from "./components/Markerposition";
 
 function App() {
   const [address, setAddress] = useState();
   const [ipAddress, setIpAddress] = useState("");
+
+  const checkIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+  const checkDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
 
   useEffect(() => {
     try {
@@ -26,11 +31,33 @@ function App() {
     }
   }, []);
 
+  const getEnteredData = async () => {
+    const res = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${
+        process.env.REACT_APP_API_KEY
+      }&${
+        checkIpAddress.test(ipAddress)
+          ? `ipAddress=${ipAddress}`
+          : checkDomain.test(ipAddress)
+          ? `domain=${ipAddress}`
+          : ""
+      }`
+    );
+    const data = await res.json();
+    setAddress(data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getEnteredData();
+    setIpAddress("");
+  };
+
   return (
     <>
-      <div className="App background w-full">
+      <div className="App background w-full h-full">
         <div className="absolute w-full -z-10">
-          <img src={background} alt="" className="w-full h-80" />
+          <img src={background} alt="" className="w-screen h-80" />
         </div>
 
         <div className="max-w-xl mx-auto p-8">
@@ -39,7 +66,7 @@ function App() {
           </h1>
 
           <form
-            //onSubmit={handleSubmit}
+            onSubmit={handleSubmit}
             autoComplete="off"
             className="w-full flex"
           >
@@ -48,7 +75,7 @@ function App() {
               name="ipaddress"
               id="ipaddress"
               placeholder="Search for any IP address or domain"
-              className="w-full py-2 px-4 rounded-l-lg"
+              className="w-full py-2 px-4 rounded-l-lg outline-none"
               value={ipAddress}
               onChange={(e) => setIpAddress(e.target.value)}
             />
@@ -60,7 +87,7 @@ function App() {
 
         {address && (
           <div
-            className=" bg-white text-center md:text-left rounded-lg shadow p-8 mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4 max-w-6xl lg:mx-auto lg:-mb-16 relative"
+            className=" bg-white text-center md:text-left rounded-lg shadow p-8 mx-8 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 max-w-6xl lg:mx-auto lg:-mb-16 -mb-16 relative"
             style={{ zIndex: 1000 }}
           >
             <div className=" lg:border-r lg:border-r-slate-400">
@@ -106,20 +133,14 @@ function App() {
           center={[address.location.lat, address.location.lng]}
           zoom={13}
           scrollWheelZoom={true}
-          style={{ height: "500px", width: "100vw" }}
+          style={{ height: "100vh", width: "100vw" }}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker
-            icon={icon}
-            position={[address.location.lat, address.location.lng]}
-          >
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+
+          <Markerposition address={address} />
         </MapContainer>
       )}
     </>
